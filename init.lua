d1531f9fc1179a1373c1f992b8d9e11a5f07e7c2
nmfirdausw <nmfirdausw@gmail.com>
2022-11-04 10:37:34 +0800

    feat(plugins): added impatient

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
-require "settings"
+local impatient_exists, impatient = pcall(require, "impatient")
+if impatient_exists then impatient.enable_profile() end
 require "bootstrap"
+require "settings"
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,13 +1,15 @@
 return {
 
+  ["lewis6991/impatient.nvim"] = {},
+
 	["nvim-lua/plenary.nvim"] = { module = "plenary" },
 
 	["wbthomason/packer.nvim"] = {
 		cmd = { "PackerSync" },
-		config = function() require "plugins" end,
+  	config = function() require "plugins" end,
 	},
 
-	['rebelot/kanagawa.nvim'] = {
+  ['rebelot/kanagawa.nvim'] = {
     as = 'kanagawa',
     config = function() require "configs.kanagawa" end,
   },
