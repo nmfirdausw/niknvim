fa2e578187e36827e2b86ed1ad2431eb13a9c280
nmfirdausw <nmfirdausw@gmail.com>
2022-11-10 22:20:30 +0800

    add telescope keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,7 @@ local set = vim.keymap.set
 -- Neo-tree File Explorer
 set('n', '<leader>e', '<cmd>Neotree reveal<cr>', { desc = 'Focus File Explorer' })
 set('n', '<leader>te', '<cmd>Neotree toggle<cr>', { desc = 'Toggle File Explorer' })
+
+-- Telescope 
+set('n', '<leader>ff', function() require("telescope.builtin").find_files() end, { desc = 'Files' })
+set('n', '<leader>fw', function() require("telescope.builtin").live_grep() end, { desc = 'Words' })
