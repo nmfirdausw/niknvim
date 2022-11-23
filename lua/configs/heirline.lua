84e470318e7d647cb597e9350ffdc43b379aef78
nmfirdausw <nmfirdausw@gmail.com>
2022-11-23 20:10:12 +0800

    fix indentation

--- lua/configs/heirline.lua
+++ lua/configs/heirline.lua
@@ -164,18 +164,18 @@ local git_status = {
     hl = { fg = "green" },
   },
   {
-      provider = function(self)
-          local count = self.status_dict.removed or 0
-          return count > 0 and ("  " .. count .. " ")
-      end,
-      hl = { fg = "red" },
+    provider = function(self)
+      local count = self.status_dict.removed or 0
+      return count > 0 and ("  " .. count .. " ")
+    end,
+    hl = { fg = "red" },
   },
   {
-      provider = function(self)
-          local count = self.status_dict.changed or 0
-          return count > 0 and ("  " .. count .. " ")
-      end,
-      hl = { fg = "orange" },
+    provider = function(self)
+      local count = self.status_dict.changed or 0
+      return count > 0 and ("  " .. count .. " ")
+    end,
+    hl = { fg = "orange" },
   },
 }
 
@@ -325,11 +325,9 @@ local navic_bar = {
       Operator = "@operator",
       TypeParameter = "@type",
     },
-    -- bit operation dark magic, see below...
     enc = function(line, col, winnr)
       return bit.bor(bit.lshift(line, 16), bit.lshift(col, 6), winnr)
     end,
-    -- line: 16 bit (65535); col: 10 bit (1023); winnr: 6 bit (63)
     dec = function(c)
       local line = bit.rshift(c, 16)
       local col = bit.band(bit.rshift(c, 6), 1023)
@@ -340,9 +338,7 @@ local navic_bar = {
   init = function(self)
     local data = require("nvim-navic").get_data() or {}
     local children = {}
-    -- create a child for each level
     for i, d in ipairs(data) do
-      -- encode line and column numbers into a single integer
       local pos = self.enc(d.scope.start.line, d.scope.start.character, self.winnr)
       local child = {
         {
@@ -350,38 +346,30 @@ local navic_bar = {
           hl = self.type_hl[d.type],
         },
         {
-          -- escape `%`s (elixir) and buggy default separators
           provider = d.name:gsub("%%", "%%%%"):gsub("%s*->%s*", ''),
-          -- highlight icon only or location name as well
-          -- hl = self.type_hl[d.type],
 
           on_click = {
-              -- pass the encoded position through minwid
-              minwid = pos,
-              callback = function(_, minwid)
-                  -- decode
-                  local line, col, winnr = self.dec(minwid)
-                  vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), {line, col})
-              end,
-              name = "heirline_navic",
+            minwid = pos,
+            callback = function(_, minwid)
+              local line, col, winnr = self.dec(minwid)
+              vim.api.nvim_win_set_cursor(vim.fn.win_getid(winnr), {line, col})
+            end,
+            name = "heirline_navic",
           },
         },
       }
-      -- add a separator only if needed
       if #data > 1 and i < #data then
         table.insert(child, {
-            provider = " > ",
-            hl = { fg = 'cyan' },
+          provider = " > ",
+          hl = { fg = 'cyan' },
         })
       end
       table.insert(children, child)
     end
-    -- instantiate the new child, overwriting the previous one
     self.child = self:new(children, 1)
   end,
-  -- evaluate the children containing navic components
   provider = function(self)
-      return self.child:eval()
+    return self.child:eval()
   end,
   hl = { fg = "white" },
   update = 'CursorMoved'
